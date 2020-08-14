fe6f57a1c2032999a7f79107abeae53623896ec6
todd <toddrob@gmail.com>
2020-08-13 21:34:20 -0400

    VER: 1.0 (Platform & MLB Game Thread Bot)

--- bots/game_threads/__init__.py
+++ bots/game_threads/__init__.py
@@ -30,7 +30,7 @@ import twitter
 
 import praw
 
-__version__ = "0.1-alpha"
+__version__ = "1.0"
 
 GENERIC_DATA_LOCK = threading.Lock()
 GAME_DATA_LOCK = threading.Lock()
--- redball/version.py
+++ redball/version.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-VERSION = "1.0-alpha_b27"
+VERSION = "1.0"
